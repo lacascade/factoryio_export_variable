a5daf5480799e7c69e93a27ae751acfcb42143dd
lacascade <aurelien.lacascade@gmail.com>
2020-02-15 19:25:59 +0100

    Update code
    Change for listing all drivers
    Read offset of driver

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # factoryio_export_variable
  Export Variable Modbus to tsv format writen in python
  The export can be used to initialize the variable table in EcoStruxure from Schneider
+
+ How to use it :
+* run the script
+* click the button "Traitement fichier"
+* select the scene saved from factoryIO
+* a .txt file is created in the same folder as the script
+* import the .txt file as the table variable in EcoStruxure
--- factoryio_export_variable.py
+++ factoryio_export_variable.py
@@ -27,6 +27,24 @@ import xml.dom.minidom
 import csv
 import time
  
+# Liste des drivers dans FactoryIO
+liste_type = ['Advantech4750',
+    'Advantech4704',
+    'AllenBradleyLogix5000',
+    'AllenBradleyMicro800',
+    'AllenBradleyMicroLogix',
+    'AllenBradleySLC5',
+    'Automgen',
+    'ControlIO',
+    'MHJ',
+    'ModbusTCPClient',
+    'ModbusTCPServer',
+    'OPCClientDA',
+    'SiemensLOGOTCP',
+    'SiemensS7300S7400TCP',
+    'SiemensS71200S71500TCP',
+    'SiemensS7PLCSIM']
+
 class App(tk.Tk):
  
     def __init__(self):
@@ -34,10 +52,10 @@ class App(tk.Tk):
         self.initWidgets()
  
     def initWidgets(self):
-        self.bt = tk.Button(self, text="Traiter fichier", command=self.openFile)
+        self.bt = tk.Button(self, text='Sélectionner le fichier', command=self.openFile)
         self.bt.grid(row=0, column=0)
  
-        self.text = tk.Text(self, width=80, height=30)
+        self.text = tk.Text(self, width=80, height=10)
         self.text.grid(row=1, column=0)
  
     def openFile(self):
@@ -48,21 +66,35 @@ class App(tk.Tk):
  
         export_to_csv(content)
         self.text.insert("end", "Traitement fini !")
-
+            
 
 def parse_xml_drivers(e):
-    # use the parse() function to load and parse an XML file
+    # utilisons la fonction parse pour analyser le DOM
     doc = xml.dom.minidom.parse(e)
     # Creons la sortie
-    b = []
+    output = []
+    # Variable tempo pour la liste des Modbus
+    driver_list = []
+    driver_name = liste_type[9]
+    driver_offset = {}
+    driver_offset_list = ['BitInputOffset','BitOutputOffset','FloatInputOffset','FloatOutputOffset','IntInputOffset','IntOutputOffset','NumericInputOffset','NumericOutputOffset']
+    # Une fois récupéré le noeud nous allons chercher le ModbusTCPClient
+    drivers = doc.getElementsByTagName(driver_name)
+    for driver in drivers[0].childNodes:
+        if ((driver.nodeType != driver.TEXT_NODE) and (driver.hasAttribute('PointIOKey'))):
+            driver_list.append({'tagname':driver.tagName,'PointIOKey':driver.getAttribute('PointIOKey')})
+        # Lecture des Offset
+        if ((driver.nodeType != driver.TEXT_NODE) and (driver.tagName == 'PointIOOffset')):
+            for driver_offset_item in driver_offset_list:
+                driver_offset[driver_offset_item]=(0,int(driver.getAttribute(driver_offset_item)))[driver.hasAttribute(driver_offset_item)]
     # allons chercher le noeud GroupIO dans la liste des objets
     GroupIOs = doc.getElementsByTagName("GroupIO")
     for GroupIO in GroupIOs:
         for node in GroupIO.childNodes:
             # Nous récupérons alors la liste des items avec le type de variable, le nom et l'adresse
             if (node.nodeType != node.TEXT_NODE):
-                b.append(create_obj_line(node))
-    return b
+                output.append(create_obj_line(node,driver_list,driver_offset))
+    return output
 
 def export_to_csv(dict_data):
     # Export au format TSV
@@ -71,7 +103,6 @@ def export_to_csv(dict_data):
     try:
         with open(csv_file, 'w') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=csv_columns, delimiter='\t')
-            #writer.writeheader()
             for data in dict_data:
                 writer.writerow(data)
     except IOError:
@@ -82,18 +113,45 @@ def prepare_ladder_var(s):
     o = "".join(i for i in s if (ord(i)<123) and ord(i)> 47)
     return o
 
-def create_obj_line(n):
-    # Nous créons une ligne
-    i = 'EBOOL' if('Binary' in n.tagName) else 'INT'
-    e = int(n.getAttribute('Address')) 
-    if('Output' in n.tagName):
-        e += 16
-    a = '%m'+str(e) if('Binary' in n.tagName) else '%mw'+str(e)
+def create_obj_line(n,c,offset_list):
+    # Nous créons une ligne à partir des attributs des noeuds
+    # Attention, il faut chercher par les key
+    k = n.getAttribute('Key')
+    PIO_item = search_PointIOKey(c, k)
+    i = ('INT','EBOOL') ['Binary' in n.tagName]
+    if PIO_item is None:
+        a = ''
+    else:
+        offset = return_offset(n,PIO_item,offset_list)
+        e = int(n.getAttribute('Address')) + offset
+        a = ('%mw'+str(e),'%m'+str(e)) ['Input' in n.tagName]
     o = {'variable' : i,
     'adresse' : a,
     'mnemonique' : prepare_ladder_var(n.getAttribute('Name')),
     'remarque' : n.getAttribute('Name')}
     return o
+
+def search_PointIOKey(values, PointIOKey):
+    # Retourne un objet cherche par les PointIOKey
+    for item in values:
+        if PointIOKey == item['PointIOKey']:
+            return item
+    return None
+
+def return_offset(n,i,offset_list):
+    # Retourne l'offset
+    o = 0
+    if(('Output' in i['tagname']) and ('Binary' in n.tagName)):
+        o = offset_list.get('BitOutputOffset',0)
+    if(('Input' in i['tagname']) and ('Binary' in n.tagName)):
+        o = offset_list.get('BitInputOffset',0)
+    if(('Output' in i['tagname']) and (('Analogue' in n.tagName) or ('Int' in n.tagName))):
+        o = offset_list.get('NumericOutputOffset',0)
+    if(('Input' in i['tagname']) and (('Analogue' in n.tagName) or ('Int' in n.tagName))):
+        o = offset_list.get('NumericInputOffset',0)
+    print(o,i['tagname'],n.tagName,offset_list)
+    return o
+
  
 #------------------------------------------------------------------------------
 if __name__ == '__main__':
